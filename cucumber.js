73c58596be2bafeb049c81831bee7d644d8dcab0
reinierRG <reinierrg.2020@gmail.com>
2025-07-25 23:58:30 -0400

    Insert reports

--- cucumber.js
+++ cucumber.js
@@ -1,29 +1,4 @@
 
-// module.exports = {
-//    default: {
-        // Especifica la ruta a tus archivos de features.
-//        paths: ['features/**/*.feature'],
-
-        // Le dice a Cucumber dónde encontrar tus definiciones de pasos y archivos de soporte.
-//        require: [
-//            'step_definitions/**/*.js', // Tus pasos de Gherkin
-//            'support/**/*.js'          // Archivos de hooks (Before, After)
-//        ],
-
-        // Define el formato de la salida en la consola y para los reportes.
-//        format: [
-//            'progress-bar', // Muestra una barra de progreso cucudurante la ejecución
-//            'json:reports/cucumber-report.json' // Genera un reporte JSON
-//        ],
-
-        // Permite pasar parámetros a tu World personalizado.
-//        worldParameters: {
-//            baseUrl: 'https://test-adl.leonardojose.dev'
-//        }
-//    }
-//}
-
-
 module.exports = {
     default: {
         // 'features' con orden específico
@@ -36,13 +11,13 @@ module.exports = {
             'features/sesionPersistente.feature'         // 6. Probar sesión persistente
         ],
 
-        // Le dice a Cucumber dónde encontrar tus definiciones de pasos y archivos de soporte.
+
         require: [
-            'step_definitions/**/*.js', // Tus pasos de Gherkin
-            'support/**/*.js'          // Archivos de hooks (Before, After)
+            'step_definitions/**/*.js', 
+            'support/**/*.js'          
         ],
 
-        // Define el formato de la salida en la consola y para los reportes.
+  
         format: ["allure-cucumberjs/reporter"],
             formatOptions: {
             resultsDir: "allure-results",
@@ -53,12 +28,12 @@ module.exports = {
             'json:reports/cucumber-report.json' // Genera un reporte JSON
         ],
         */
-        // Permite pasar parámetros a tu World personalizado.
+    
         worldParameters: {
             baseUrl: 'https://test-adl.leonardojose.dev'
         },
 
-        // CRÍTICO: Sin paralelización para mantener orden secuencial
+
         parallel: 1
     }
 };
